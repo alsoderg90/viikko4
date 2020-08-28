1aafadffed72d39b88f982560ec4df79221647eb
alexander <alsoderg@student.jyu.fi>
2020-08-28 14:51:10 +0300

    4.6, step4

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -27,8 +27,25 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
-    const result = a
+    const authors = blogs.map(name => name = name.author)
+    const result = []
 
+    for (i=0; i<authors.length; i++) {
+        const temp = result.map(name => name = name.author)
+
+        if (!temp.includes(authors[i])) {
+        result.push({ author: authors[i], likes: 1 })
+        }
+        else {
+            const index = temp.findIndex(name => name == authors[i])
+            result[index] = {author: result[index].author, likes: result[index].likes+1}
+        }
+    }
+
+    console.log(authors, result)
+    return  result.reduce((prev,curr) => {
+       return (prev.likes > curr.likes) ? prev : curr
+    })
 }
 
 module.exports = {
