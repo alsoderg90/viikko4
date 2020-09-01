487349a046433081882f28bd3530c48ad4ab8f6e
alexander <alsoderg@student.jyu.fi>
2020-09-02 02:48:48 +0300

    2.20

--- blogilista/app.js
+++ blogilista/app.js
@@ -26,6 +26,7 @@ mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: t
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
