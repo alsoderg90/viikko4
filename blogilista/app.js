b37b2daa6ea3f29f667412608eff2f20eee0bd53
alexander <alsoderg@student.jyu.fi>
2020-09-01 16:41:48 +0300

    4.15

--- blogilista/app.js
+++ blogilista/app.js
@@ -5,6 +5,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 mongoose.set('useFindAndModify', false)
 
@@ -24,6 +25,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 
